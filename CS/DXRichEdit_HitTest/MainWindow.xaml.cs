bc4fd3ca43a2b27f50e60a87aed9c060cd77241d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXRichEdit_HitTest/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+#region usings
+using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.Utils;
+using DevExpress.XtraRichEdit.API.Native;
+#endregion #usings
+
+namespace DXRichEdit_HitTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            richEditControl1.Loaded += new RoutedEventHandler(richEditControl1_Loaded);
+        }
+
+        void richEditControl1_Loaded(object sender, RoutedEventArgs e)
+        {
+            richEditControl1.LoadDocument("sample.docx", DocumentFormat.OpenXml);
+            biHitTest.CheckedChanged += new DevExpress.Xpf.Bars.ItemClickEventHandler(biHitTest_CheckedChanged);
+            richEditControl1.Options.Bookmarks.Visibility = RichEditBookmarkVisibility.Visible;
+            richEditControl1.Options.Bookmarks.Color = System.Drawing.Color.Red;
+        }
+
+        void biHitTest_CheckedChanged(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            if (this.biHitTest.IsChecked == true)
+            {
+                richEditControl1.MouseMove += new MouseEventHandler(richEditControl1_MouseMove);
+            }
+            else {
+                richEditControl1.MouseMove -= new MouseEventHandler(richEditControl1_MouseMove);
+            }
+        }
+
+        #region #hittest
+        void richEditControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point point = e.GetPosition((UIElement)richEditControl1);
+            System.Drawing.Point pt = Units.PixelsToDocuments(new System.Drawing.Point((int)point.X,
+        (int)point.Y), richEditControl1.DpiX, richEditControl1.DpiY);
+            DocumentPosition pos = richEditControl1.GetPositionFromPoint(pt);
+
+            BookmarkCollection bmCollection = richEditControl1.Document.Bookmarks;
+            textBlock1.Text = String.Empty; //Clear a text block displaying bookmark info
+            
+            if (pos != null)
+            {
+                foreach (Bookmark bm in bmCollection)
+                {
+                    if (bm.Range.Contains(pos))
+                    {
+                        Bookmark bmHovered = bm;
+                        richEditControl1.Document.SelectBookmark(bmHovered);
+                        ShowBookmarkInTextBlock(bmHovered);
+                        break;
+                    }
+                }
+            }           
+        }
+        #endregion #hittest
+
+        private void ShowBookmarkInTextBlock(Bookmark bm)
+        {
+            string textFragment = richEditControl1.Document.GetText(bm.Range);
+            textBlock1.Text = String.Format("You are over a bookmark '{0}' containing text: '{1}'", bm.Name, textFragment);
+        }
+
+
+
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# DXRichEdit for WPF: How to use hit testing
+
+
+<p>This example illustrates handling the <strong>MouseMove </strong>event of the RichEditControl and using the <a href="http://documentation.devexpress.com/#WPF/DevExpressXpfRichEditRichEditControl_GetPositionFromPointtopic"><u>GetPositionFromPoint</u></a> method to obtain the corresponding position in the document. <br />
+If the position under the mouse cursor belongs to a <a href="http://documentation.devexpress.com/#WPF/CustomDocument9103"><u>bookmark</u></a>, this bookmark is selected using the <a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraRichEditAPINativeSubDocument_SelectBookmarktopic"><u>SelectBookmark</u></a> method and the bookmark's name and a text fragment are displayed in the text box below.</p>
+
+<br/>
+
+
